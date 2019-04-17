96c70b909251dbc2a3058a0f07006f092263390e
Jéssica Ayumi Uehara <jessicayumiget@gmail.com>
2019-04-17 11:09:41 -0300

    Alterando classe ClienteDAO

--- lesson-11/project-example/src/main/java/br/edu/utfpr/dao/ClienteDAO.java
+++ lesson-11/project-example/src/main/java/br/edu/utfpr/dao/ClienteDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 import br.edu.utfpr.dto.ClienteDTO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import lombok.extern.java.Log;
 
 @Log
@@ -26,6 +29,91 @@ public class ClienteDAO {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    public boolean create(ClienteDTO cliente) throws SQLException {
+        PreparedStatement stmt = null;
+        Connection con = null;
+        try {
+            stmt = con.prepareStatement("INSERT INTO cliente (nome, telefone, idade, limiteCredito, id_pais) values(?,?,?,?,?)");
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setInt(3, cliente.getIdade());
+            stmt.setDouble(4, cliente.getLimiteCredito());
+            stmt.setInt(5, cliente.getPais().getId());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Erro ao inserir: " + ex);
+            return false;
+        } finally {
+            con.close();
+        }
+    }
+
+    public boolean update(ClienteDTO cliente) throws SQLException {
+        PreparedStatement stmt = null;
+        Connection con = null;
+        String sql = "UPDATE cliente SET nome = ?, telefone = ?, idade = ?, limiteCredito = ?, id_pais = ? WHERE id = ?";
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setInt(3, cliente.getIdade());
+            stmt.setDouble(4, cliente.getLimiteCredito());
+            stmt.setInt(5, cliente.getPais().getId());
+            stmt.executeUpdate();
+
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Erro ao atualizar: " + ex);
+            return false;
+        } finally {
+            con.close();
+        }
+    }
+
+    public boolean delete(int id) throws SQLException {
+        PreparedStatement stmt = null;
+        Connection con = null;
+        try {
+            stmt = con.prepareStatement("DELETE FROM cliente WHERE id = ?");
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Erro ao deletar: " + ex);
+            return false;
+        } finally {
+            con.close();
+        }
+    }
+
+    public ClienteDTO search(String nome) throws SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Connection con = null;
+        try {
+            stmt = con.prepareStatement("SELECT id, nome, telefone, idade, limiteCredito, id_pais FROM cliente WHERE name LIKE concat('%', ?,'%')");
+            stmt.setString(1, nome);
+            rs = stmt.executeQuery();
+            ClienteDTO cliente = null;
+            while (rs.next()) {
+                cliente.setId(rs.getInt(1));
+                cliente.setNome(rs.getString(2));
+                cliente.setTelefone(rs.getString(3));
+                cliente.setIdade(rs.getInt(4));
+                cliente.setLimiteCredito(rs.getDouble(5));
+                cliente.setPais(rs.getInt(6));
+            }
+            return cliente;
+        } catch (SQLException ex) {
+            System.err.println("Erro ao encontrar: " + ex);
+            return null;
+        } finally {
+            con.close();
         }
     }
 }
